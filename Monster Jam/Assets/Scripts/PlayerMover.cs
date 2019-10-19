a05b69e434551a19761cabf54e8f9e244562d948
Sam Trapp <trapp.s.e@gmail.com>
2019-10-19 15:55:01 -0600

    Adding sprint.

--- Monster Jam/Assets/Scripts/PlayerMover.cs
+++ Monster Jam/Assets/Scripts/PlayerMover.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(PlayerAnimator))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMover : MonoBehaviour
 {
 	Rigidbody body = null;
@@ -13,13 +13,8 @@ public class PlayerMover : MonoBehaviour
 	Vector3 verticalAxis = Vector3.up;
 	Vector3 rotationAxis = Vector3.forward;
 	[SerializeField]
-	float maxSpeed = 10f;
-	[SerializeField]
-	float acceleration = 10f;
-	[SerializeField, Tooltip("Degrees Per Frame")]
-	float turnSpeed = 360f;
-	[SerializeField]
-	float dragFactor = 0.8f;
+	MoveStats defaultStats = null;
+	MoveStats stats = null;
 	bool moving = false;
 
 	Vector3 externalForce = Vector3.zero;
@@ -30,6 +25,7 @@ public class PlayerMover : MonoBehaviour
 		anim = GetComponent<PlayerAnimator>();
 
 		rotationAxis = Vector3.Cross(horizontalAxis, verticalAxis);
+		stats = defaultStats;
 	}
 
 	private void FixedUpdate()
@@ -39,12 +35,16 @@ public class PlayerMover : MonoBehaviour
 		var horizontal = Input.GetAxis($"Horizontal");
 		var vertical = Input.GetAxis($"Vertical");
 
+
 		if (Mathf.Abs(horizontal) > Helper.Epsilon || Mathf.Abs(vertical) > Helper.Epsilon)
 		{
 			if (moving == false)
 			{
 				moving = true;
-				anim.StartAnimation("move");
+				if (anim != null)
+				{
+					anim.StartAnimation("move");
+				}
 			}
 		}
 		else
@@ -52,39 +52,45 @@ public class PlayerMover : MonoBehaviour
 			if (moving == true)
 			{
 				moving = false;
-				anim.StopAnimation("move");
+				if (anim != null)
+				{
+					anim.StopAnimation("move");
+				}
 			}
 		}
 
 		body.AddForce(externalForce, ForceMode.Impulse);
 
-		var internalForce = (((horizontalAxis * horizontal) + (verticalAxis * vertical))).normalized * acceleration;
+		var internalForce = (((horizontalAxis * horizontal) + (verticalAxis * vertical))).normalized * stats.acceleration;
 
 		if (internalForce.sqrMagnitude > 0)
 		{
 			var lookAt = internalForce.normalized;
-
 			var angle = Mathf.Acos(Vector3.Dot(Vector3.forward, lookAt)) * Mathf.Rad2Deg;
 			if (Vector3.Dot(Vector3.right, lookAt) > 0)
 			{
 				angle *= -1;
 			}
 
-			body.rotation = Quaternion.RotateTowards(body.rotation, Quaternion.Euler(rotationAxis * angle), turnSpeed);
+			// If attempting to turn fully around, randomly choose a direction to turn.
+			if (Mathf.Abs(Vector3.Dot(transform.forward, lookAt) + 1) < Helper.Epsilon && Random.Range(0f, 1f) < 0.5f)
+			{
+				angle = -180;
+			}
+
+			body.rotation = Quaternion.RotateTowards(body.rotation, Quaternion.Euler(rotationAxis * angle), stats.turnSpeed);
 		}
 
-		if (body.velocity.sqrMagnitude < (maxSpeed * maxSpeed))
+		body.AddForce(internalForce);
+
+		if (body.velocity.sqrMagnitude > (stats.maxSpeed * stats.maxSpeed))
 		{
-			body.AddForce(internalForce);
+			body.velocity = body.velocity.normalized * stats.maxSpeed;
 		}
 
-		if ((internalForce.sqrMagnitude + externalForce.sqrMagnitude) > Helper.Epsilon)
+		if ((internalForce.sqrMagnitude + externalForce.sqrMagnitude) < Helper.Epsilon)
 		{
-			body.velocity *= dragFactor;
-		}
-		else
-		{
-			body.velocity = Vector3.zero;
+			body.velocity *= stats.dragFactor;
 		}
 
 		externalForce = Vector3.zero;
@@ -94,4 +100,27 @@ public class PlayerMover : MonoBehaviour
 	{
 		externalForce += force;
 	}
+
+	public void SetMoveStats(MoveStats newStats)
+	{
+		stats = newStats;
+	}
+
+	public void ResetMoveStats()
+	{
+		stats = defaultStats;
+	}
+}
+
+[System.Serializable]
+public class MoveStats
+{
+	[SerializeField]
+	public float maxSpeed = 1000f;
+	[SerializeField]
+	public float acceleration = 100f;
+	[SerializeField, Tooltip("Degrees Per Frame")]
+	public float turnSpeed = 15f;
+	[SerializeField]
+	public float dragFactor = 0.8f;
 }
