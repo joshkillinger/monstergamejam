a1fa3506688bba89698b2d1656367df21426c690
Sam Trapp <trapp.s.e@gmail.com>
2019-10-20 15:34:01 -0600

    Fixing home base stuff after some changes

--- Monster Jam/Assets/Scripts/InventoryManager.cs
+++ Monster Jam/Assets/Scripts/InventoryManager.cs
@@ -126,6 +126,8 @@ public class InventoryManager : MonoBehaviour
 			var category = validCategories[Random.Range(0, validCategories.Count)];
 			var item = category[Random.Range(0, category.Count)];
 			category.Remove(item);
+			Debug.Log("REMOVED FROM CATEGORY " + category.Count);
+			++Version;
 			return item;
 		}
 
