ca4f6f014b2192b115b52d7f03861e88fa9a81b1
Sam Trapp <trapp.s.e@gmail.com>
2019-10-20 11:13:15 -0600

    Hitting home base

---
+++ Monster Jam/Assets/Scripts/Homebase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homebase : MonoBehaviour
+{
+	string playerTag = "Player";
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("HIT");
+		if (other.CompareTag(playerTag))
+		{
+			Debug.Log("Player");
+			//var controller = GameObject.FindGameObjectWithTag("GameController");
+			//controller.GetComponent<InventoryManager>().PickUp(this);
+
+			//GetComponent<Animator>().SetTrigger(PickupTrigger);
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		Debug.Log("STAY");
+		if (other.CompareTag(playerTag))
+		{
+			Debug.Log("Player");
+			//var controller = GameObject.FindGameObjectWithTag("GameController");
+			//controller.GetComponent<InventoryManager>().PickUp(this);
+
+			//GetComponent<Animator>().SetTrigger(PickupTrigger);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.Log("EXIT");
+		if (other.CompareTag(playerTag))
+		{
+			Debug.Log("Player");
+			//var controller = GameObject.FindGameObjectWithTag("GameController");
+			//controller.GetComponent<InventoryManager>().PickUp(this);
+
+			//GetComponent<Animator>().SetTrigger(PickupTrigger);
+		}
+	}
+}
