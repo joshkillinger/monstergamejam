1bec161bbf960be9a9eba05faf443b5cc3027573
Sam Trapp <trapp.s.e@gmail.com>
2019-10-20 10:24:42 -0600

    Adding score manager and view for UI.

---
+++ Monster Jam/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+	private int score = 0;
+	public int Score => score;
+
+	public void AddScore(int amount)
+	{
+		score += amount;
+	}
+}
