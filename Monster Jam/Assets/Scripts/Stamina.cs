7bb9e94733540601f1fcc64b5a4ed58f970be56d
Sam Trapp <trapp.s.e@gmail.com>
2019-10-19 20:40:11 -0600

    Adding Stamina and using it sprint.

---
+++ Monster Jam/Assets/Scripts/Stamina.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamina : MonoBehaviour
+{
+	private float total = 1f;
+	public float Total => total;
+
+	public bool Consume(float amount)
+	{
+		if (total >= amount)
+		{
+			alterStamina(-amount);
+			return true;
+		}
+		return false;
+	}
+
+	public void Repenlish(float amount)
+	{
+		alterStamina(amount);
+	}
+
+	private void alterStamina(float amount)
+	{
+		total = Mathf.Clamp01(total + amount);
+	}
+}
---
+++ Monster Jam/Assets/Scripts/StaminaRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaRenderer : MonoBehaviour
+{
+	Stamina stamina = null;
+
+	[SerializeField]
+	Text text;
+
+	private void Start()
+	{
+		stamina = GameObject.FindWithTag("Player").GetComponentInChildren<Stamina>();
+	}
+
+	private void Update()
+	{
+		if (stamina != null)
+		{
+			if (text != null)
+			{
+				text.text = "" + (int)(stamina.Total * 100);
+			}
+		}
+	}
+}
