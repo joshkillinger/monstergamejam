ee589491e19176eb7548d20f38b95203e1a67831
Sam Trapp <trapp.s.e@gmail.com>
2019-10-20 16:15:06 -0600

    Made start scene

---
+++ Monster Jam/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+	[SerializeField]
+	string gameScene;
+
+	public void StartMatch()
+	{
+		SceneManager.LoadSceneAsync(gameScene);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
