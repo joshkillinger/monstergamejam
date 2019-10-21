50788112e9eee4dc071ae6508fbd69cc1841f35d
Sam Trapp <trapp.s.e@gmail.com>
2019-10-20 21:19:39 -0600

    Refixing smell hinter

--- Monster Jam/Assets/Scripts/UI/PickupHinter.cs
+++ Monster Jam/Assets/Scripts/UI/PickupHinter.cs
@@ -4,88 +4,143 @@ using UnityEngine;
 
 public class PickupHinter : MonoBehaviour
 {
-    public float wiggleSpeed = 2;
-    public float minDistForHint = 1000;
-    public float maxDistForHint = 5000;
-    public float MaxScale;
+	public float wiggleSpeed = 2;
+	class SmellLine
+	{
+		public GameObject root = null;
+		public LineRenderer line = null;
+		public Transform target = null;
+		public bool faded = false;
+	}
 
-    public LineRenderer line;
-    public float length = 20;
+	public float fadeDuration = 1;
+	public float minDistForHint = 1000;
+	public float maxDistForHint = 5000;
+	public float MaxScale;
 
-    List<float> scales;
-    List<float> times;
+	public GameObject lineRootTemplate;
+	public float length = 20;
+	List<SmellLine> smellLines = new List<SmellLine>();
 
-    private bool initialized = false;
+	List<float> scales;
+	List<float> times;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        scales = new List<float>(line.positionCount);
-        times = new List<float>(line.positionCount);
+	private bool initialized = false;
 
-        for (int i = 0; i < line.positionCount; ++i)
-        {
-            scales.Add(Random.value * MaxScale);
-            times.Add(Random.value);
-            line.SetPosition(i, new Vector3(Mathf.Sin(times[i] + Time.time * 2) * scales[i], 0, i * length / line.positionCount));
-        }
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		var lineTemplate = lineRootTemplate.GetComponentInChildren<LineRenderer>();
+		scales = new List<float>(lineTemplate.positionCount);
+		times = new List<float>(lineTemplate.positionCount);
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (initialized)
-        {
-            //wiggle
-            for (int i = 0; i < line.positionCount; ++i)
-            {
-                line.SetPosition(i, new Vector3(Mathf.Sin(times[i] + Time.time) * scales[i], 0, line.GetPosition(i).z));
-            }
+		for (int i = 0; i < lineTemplate.positionCount; ++i)
+		{
+			scales.Add(Random.value * MaxScale);
+			times.Add(Random.value);
+			lineTemplate.SetPosition(i, new Vector3(Mathf.Sin(times[i] + Time.time * 2) * scales[i], 0, i * length / lineTemplate.positionCount));
+		}
+	}
 
-            //rotation
-            var target = getTarget();
-            if (target == null)
-            {
-                line.gameObject.SetActive(false);
-            }
-            else
-            {
-                line.gameObject.SetActive(true);
-                var toTarget = (target.position - transform.position).normalized;
-                var angle = Vector3.SignedAngle(Vector3.forward, toTarget, Vector3.up);
-                transform.rotation = Quaternion.Euler(0, angle, 0);
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		var bestSmell = getBestSmellLine();
 
-    List<GameObject> items;
+		foreach (var smell in smellLines)
+		{
+			//wiggle
+			for (int i = 0; i < smell.line.positionCount; ++i)
+			{
+				smell.line.SetPosition(i, new Vector3(Mathf.Sin(times[i] + Time.time) * scales[i], 0, smell.line.GetPosition(i).z));
+			}
 
-    public void SetItems(List<GameObject> items)
-    {
-        this.items = items;
-        initialized = true;
-    }
+			//rotation
+			if (smell.target != null)
+			{
+				var toTarget = (smell.target.position - transform.position).normalized;
+				var angle = Vector3.SignedAngle(Vector3.forward, toTarget, Vector3.up);
+				smell.root.transform.rotation = Quaternion.Euler(0, angle, 0);
+			}
 
-    private Transform getTarget()
-    {
-        Transform closest = null;
-        float closestDist = float.MaxValue;
+			var color = smell.line.startColor;
+			if (smell == bestSmell)
+			{
+				color.a = 1f;
+			}
+			else
+			{
+				color.a -= fadeDuration * Time.deltaTime;
+				smell.faded = color.a <= 0;
+			}
+			smell.line.startColor = color;
+			smell.line.endColor = color;
+		}
 
-        foreach (var item in items)
-        {
-            if (item == null || item.transform == null)
-            {
-                continue;
-            }
+		for (int i = smellLines.Count - 1; i >= 0; i--)
+		{
+			if (smellLines[i].faded)
+			{
+				Destroy(smellLines[i].root);
+				smellLines.RemoveAt(i);
+			}
+		}
+	}
 
-            var dist = (item.transform.position - transform.position).sqrMagnitude;
-            if (dist > minDistForHint && dist < maxDistForHint && dist < closestDist)
-            {
-                closestDist = dist;
-                closest = item.transform;
-            }
-        }
+	List<GameObject> items;
 
-        return closest;
-    }
+	public void SetItems(List<GameObject> items)
+	{
+		this.items = items;
+		initialized = true;
+	}
+
+	private SmellLine getBestSmellLine()
+	{
+		Transform closest = null;
+		float closestDist = float.MaxValue;
+
+		foreach (var item in items)
+		{
+			if (item == null || item.transform == null)
+			{
+				continue;
+			}
+
+			var dist = (item.transform.position - transform.position).sqrMagnitude;
+			if (dist > minDistForHint && dist < maxDistForHint && dist < closestDist)
+			{
+				closestDist = dist;
+				closest = item.transform;
+			}
+		}
+
+		SmellLine bestSmell = null;
+		foreach (var smell in smellLines)
+		{
+			if (closest == smell.target)
+			{
+				bestSmell = smell;
+				break;
+			}
+		}
+
+		if (bestSmell != null)
+		{
+			return bestSmell;
+		}
+		else
+		{
+			var root = Instantiate(lineRootTemplate, lineRootTemplate.transform.parent);
+			root.gameObject.SetActive(true);
+			var newSmell = new SmellLine()
+			{
+				root = root,
+				line = root.GetComponentInChildren<LineRenderer>(),
+				target = closest
+			};
+			smellLines.Add(newSmell);
+			return newSmell;
+		}
+	}
 }
